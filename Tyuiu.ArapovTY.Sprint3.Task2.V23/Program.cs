4278232352eb2d3c01b9fb8a96b59ee5ebfe9765
Тимофей Арапов <Tyuiu.ArapovTY@outlook.com>
2024-10-13 13:18:57 +0500

    Task2

---
+++ Tyuiu.ArapovTY.Sprint3.Task2.V23/Program.cs
@@ -0,0 +1,36 @@
+﻿using Tyuiu.ArapovTY.Sprint3.Task2.V23.Lib;
+namespace Tyuiu.ArapovTY.Sprint3.Task2.V23
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #3 | Выполнил: Арапов Т. Я. | АСОиУб-24-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #3                                                               *");
+            Console.WriteLine("* Тема: Оператор цикла do-while                                           *");
+            Console.WriteLine("* Задание #2                                                              *");
+            Console.WriteLine("* Вариант #23                                                             *");
+            Console.WriteLine("* Выполнил: Арапов Тимофей Яковлевич | АСОиУб-24-1                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, используя цикл do...while, которая вычисляет сумму  *");
+            Console.WriteLine("* ряда по формуле.                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            double value = 1.5;
+            int startValue = 1;
+            int stopValue = 13;
+            Console.WriteLine("Переменная X= " + value);
+            Console.WriteLine("Старт шага = " + startValue);
+            Console.WriteLine("Конец шага = " + stopValue);
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("Сумма ряда = " + ds.GetSumSeries(value, startValue, stopValue));
+            Console.ReadKey();
+        }
+    }
+}
